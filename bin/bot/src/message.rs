59b0fa8f1e43a18d3677ddd3a229972284d550bd
Brian Koehler <briandkoehler@gmail.com>
2024-12-30 18:44:33 -0800

    add more placeholder templates

--- bin/bot/src/message.rs
+++ bin/bot/src/message.rs
@@ -2,7 +2,12 @@ use rand::seq::SliceRandom;
 use the_collector_db::model;
 
 // TODO: Load from configuration file
-const TEMPLATES: [&str; 1] = ["%s just died %d times"];
+const TEMPLATES: [&str; 4] = [
+    "%s just died **%d times**.",
+    "Solid **%d bomb** by %s.",
+    "**BREAKING NEWS:** %S INTS ANOTHER GAME WITH **%d DEATHS**.",
+    "**NEWS FLASH:** %S DROPS A **%d DEATH** GAME."
+];
 
 pub struct MessageBuilder {
     templates: Vec<String>,
@@ -22,6 +27,7 @@ impl MessageBuilder {
             .choose(&mut rand::thread_rng())
             .expect("Templates is unexpectedly empty");
         let message = message.replace("%s", &summoner_match.puuid);
+        let message = message.replace("%S", &summoner_match.puuid.to_uppercase());
         message.replace("%d", &summoner_match.deaths.to_string())
     }
 }
