9b7a06f068e418a105b6fd19ed263ac9f66f37f7
Brian Koehler <briandkoehler@gmail.com>
2025-01-03 00:18:08 -0800

    improve match data handler error handling

--- bin/the-collector/src/handler/match_data.rs
+++ bin/the-collector/src/handler/match_data.rs
@@ -32,35 +32,51 @@ impl MatchDataHandler {
     #[tracing::instrument]
     pub async fn start(mut self) {
         loop {
-            let data = self
-                .rx_channel
-                .recv()
-                .await
-                .expect("Receiving channel closed unexpectedly");
-            debug!("Received Match data: {:?}", data.metadata.match_id);
-
-            // Insert general match data into DB
-            // TODO: Batch inserts and/or DB jobs queue
-            if let Err(e) = self.db_conn.insert_match(&data).await {
-                error!("Failed to insert match into database: {e:?}");
-            }
-
-            for puuid in &data.metadata.participants {
-                // Insert followed info into DB
-                if self.db_conn.get_summoner(puuid).await.unwrap().is_some() {
-                    if let Err(e) = self.db_conn.insert_summoner_match(puuid, &data).await {
-                        error!("Failed to insert summoner match data into database: {e:?}");
-                    }
-
-                    // TODO: Avoid cloning?
-                    let message = SummonerMatchQuery {
-                        puuid: puuid.clone(),
-                        match_id: data.metadata.match_id.clone(),
-                    };
-                    info!("Sending match query: {message:?}");
-                    self.publisher.publish(message).await.unwrap();
-                }
+            match self.run().await {
+                Ok(count) => info!("Inserted {count} summoner matches"),
+                Err(e) => error!("Match Data Handler error: {e:?}"),
             }
         }
+    }
+
+    async fn run(&mut self) -> anyhow::Result<u8> {
+        // Receive data
+        let data = self
+            .rx_channel
+            .recv()
+            .await
+            .expect("Receiving channel closed unexpectedly");
+        debug!("Received Match data: {:?}", data.metadata.match_id);
+
+        // Insert general match data into DB
+        // TODO: Batch inserts and/or DB jobs queue
+        if let Err(e) = self.db_conn.insert_match(&data).await {
+            // TODO: Retry
+            anyhow::bail!("Failed to insert match into database: {e:?}");
+        }
+
+        // Insert followed info into DB
+        let mut count = 0;
+        for puuid in &data.metadata.participants {
+            if self.db_conn.get_summoner(puuid).await?.is_none() {
+                continue;
+            }
+
+            if let Err(e) = self.db_conn.insert_summoner_match(puuid, &data).await {
+                error!("Failed to insert summoner match data into database: {e:?}");
+                continue;
+            }
+
+            // TODO: Avoid cloning?
+            let message = SummonerMatchQuery {
+                puuid: puuid.clone(),
+                match_id: data.metadata.match_id.clone(),
+            };
+            debug!("Sending match query: {message:?}");
+            self.publisher.publish(message).await?;
+            count += 1;
+        }
+
+        Ok(count)
     }
 }
