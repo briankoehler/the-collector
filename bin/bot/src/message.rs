bfcf45214bf1b425d3b58e22d2452feda6b0b56f
Brian Koehler <briandkoehler@gmail.com>
2025-01-02 16:57:45 -0800

    add additional message templates

--- bin/bot/src/message.rs
+++ bin/bot/src/message.rs
@@ -2,11 +2,17 @@ use rand::seq::SliceRandom;
 use the_collector_db::model;
 
 // TODO: Load from configuration file
-const TEMPLATES: [&str; 4] = [
-    "%s just died **%d times**.",
+const TEMPLATES: [&str; 10] = [
+    "%s just died **%d times**. Wow!",
     "Solid **%d bomb** by %s.",
+    "**%d death** game coming from %s. Nice.",
+    "Just a little bit of limit testing by %s, resulting in **%d deaths.**",
+    "Mr. Inty Pants %s just inted **%d times!**",
+    "Yikes, **%d deaths** for %s that last match.",
+    "What a game by %s! **%k kills and %d deaths!**",
     "**BREAKING NEWS:** %S INTS ANOTHER GAME WITH **%d DEATHS**.",
     "**NEWS FLASH:** %S DROPS A **%d DEATH** GAME.",
+    "Holy moly - **%d DEATHS** BY %S!!"
 ];
 
 pub struct MessageBuilder {
@@ -30,8 +36,10 @@ impl MessageBuilder {
             .templates
             .choose(&mut rand::thread_rng())
             .expect("Templates is unexpectedly empty");
-        let message = message.replace("%s", &summoner.game_name);
-        let message = message.replace("%S", &summoner.game_name.to_uppercase());
-        message.replace("%d", &summoner_match.deaths.to_string())
+        message.replace("%s", &summoner.game_name)
+            .replace("%S", &summoner.game_name.to_uppercase())
+            .replace("%d", &summoner_match.deaths.to_string())
+            .replace("%d", &summoner_match.deaths.to_string())
+            .replace("%k", &summoner_match.kills.to_string())
     }
 }
